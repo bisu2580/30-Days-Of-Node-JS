621e278420b37dc427400b19433c992d449af89f
Biswajit Sahoo <biswatian123@gmail.com>
2024-02-10 23:13:29 +0530

    Modified Day 10

--- Feb 10/app.js
+++ Feb 10/app.js
@@ -2,11 +2,19 @@ const express = require("express")
 const path = require("path")
 var app = express()
 
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "index.html"))
-})
+function staticFileServer(req, res) {
+	// Your implementation here
+	if (req.url == "/") res.sendFile(path.join
+	(__dirname, "public", "index.html"))
+	else res.sendFile(path.join
+	(__dirname, "public/styles", "style.css"))
+}
+app.get("/", staticFileServer)
+// app.get("/styles/style.css", staticFileServer)
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join
+(__dirname, "public")))
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log
+(`Listening on ${PORT}`))
