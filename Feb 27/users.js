c32248ee58bbd788a7d8cbcf679f162495616917
Biswajit Sahoo <biswatian123@gmail.com>
2024-02-28 03:19:25 +0530

    Completed Day 27

---
+++ Feb 27/users.js
@@ -0,0 +1,6 @@
+const users = [
+	{ id: 1, username: "Biswajit", password: "biswa2001", role: "admin" },
+	{ id: 2, username: "Amit", password: "amit2005", role: "user" },
+	{ id: 3, username: "Hola", password: "hola@2010", role: "user" },
+]
+module.exports = users
